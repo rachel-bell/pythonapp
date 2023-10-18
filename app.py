e240f77152d77ba2d9bcb2af0b79a819b399d531
rachel-bell <rbell15426@gmail.com>
2023-10-18 20:31:57 +1300

    Create app.py

---
+++ app.py
@@ -0,0 +1,31 @@
+import streamlit as st
+import boto3
+import json
+
+#%%writefile app.py
+
+def make_prediction(image_bytes):
+    runtime = boto3.Session().client('sagemaker-runtime')
+    response = runtime.invoke_endpoint(EndpointName='Gram-Image-Classifier',
+                                       ContentType = 'application/x-image',
+                                       Body=image_bytes)
+    result = json.loads(response['Body'].read().decode())
+    return result
+
+st.title('Gram Stain Image Classifier')
+uploaded_file = st.file_uploader('Choose an image to classify…', type = ['jpg', 'jpeg', 'png'])
+if uploaded_file is not None:
+    image_bytes = uploaded_file.read()
+    result = make_prediction(image_bytes)
+    label = {result['predicted_label']}
+    st.write("###")
+    st.write("Classifying...")
+    #st.write(f'Predicticted label: {result['predicted_label']}')
+    #st.write(f'Probability: {result['probability']})
+    if label == "GN":
+        st.write("The Gram stain contains Gram negative bacteria")
+        st.write(f"Probability: {result['probability']}")
+
+    else:
+        st.write("The Gram stain contains Gram positive bacteria")
+        st.write(f"Probability: {result['probability']}")
