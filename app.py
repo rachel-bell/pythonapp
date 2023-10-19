16a6b3a1fe47048fad5f92bb8276f0f0eddbac58
rachel-bell <44662765+rachel-bell@users.noreply.github.com>
2023-10-19 17:46:09 +1300

    Update app.py

--- app.py
+++ app.py
@@ -17,10 +17,10 @@ uploaded_file = st.file_uploader('Choose an image to classify…', type = ['jpg'
 if uploaded_file is not None:
     image_bytes = uploaded_file.read()
     result = make_prediction(image_bytes)
-    if result < 0.5:
-        predicted_label = 'Gram Negative'
+    if result['predicted_label'] < 0.5:
+        label = 'Gram Negative'
     else:
-        predicted_label = 'Gram Positive'
+        label = 'Gram Positive'
     label = {result['predicted_label']}
     st.write("###")
     st.write("Classifying...")
