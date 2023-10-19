c01c6ae6e45dd1e33e42dbe3ad6f0934b59e3427
rachel-bell <44662765+rachel-bell@users.noreply.github.com>
2023-10-19 18:08:26 +1300

    Update app.py

--- app.py
+++ app.py
@@ -17,16 +17,15 @@ uploaded_file = st.file_uploader('Choose an image to classify…', type = ['jpg'
 if uploaded_file is not None:
     image_bytes = uploaded_file.read()
     result = make_prediction(image_bytes)
-    st.write("---")
-    st.write(result) #TESTING
-    if result.probabilities[0] > result.probabilities[1]:
+    probabilities = [result["probabilities"][0], result["probabilities"][1]]
+
+    if probabilities[0] > probabilities[1]:
         bacteria_class = "GN"
         bacteria_class_name = "Gram Negative bacteria"
     else:
         bacteria_class = "GP"
         bacteria_class_name = "Gram Positive bacteria"
-
+        
     
     st.write(f"The Gram stain contains {bacteria_class_name}")
     st.write(f"Probability of being in the {bacteria_class_name}: {probabilities[0] if bacteria_class == 'GN' else probabilities[1]}")
-
